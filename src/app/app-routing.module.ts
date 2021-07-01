2b50b72f035b78c726cd3b4307a4fa30f4de6eff
olemalik <olemalik@gmail.com>
2021-07-02 02:02:39 +0530

    Add files via upload
    Innit Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CustomerComponent } from './customer/customer.component';
+
+const routes: Routes = [
+  { path: '', component: CustomerComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
